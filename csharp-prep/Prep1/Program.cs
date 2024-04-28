1fe9928a6bad80321acf4387384d73520f3fa107
PrimePine <kokotlaw@gmail.com>
2024-04-27 18:02:50 -0600

    Work done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your First Name: ");
+        string first = Console.ReadLine();
+        Console.Write("Please enter your last name: ");
+        string Last = Console.ReadLine();
+        Console.WriteLine($"Your name is {Last}, {first} {Last}.");
     }
 }
