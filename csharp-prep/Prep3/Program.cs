e66a7f48031635d3dc510ca7f7cf282e61ccd03d
PrimePine <kokotlaw@gmail.com>
2024-04-29 11:29:45 -0600

    Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomgenerator = new Random();
+        int magicnumber = randomgenerator.Next(1,101);
+        int guess = -1;
+        while (guess != magicnumber)
+        {
+            Console.Write("What is your guess");
+            guess=int.Parse(Console.ReadLine());
+
+            if (magicnumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicnumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it");
+            }
+        }
     }
 }
